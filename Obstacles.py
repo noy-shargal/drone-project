e850efcf1c4a0e4cd5587b8a1dd1444c3d35f121
Noy Shargal <64549524+noy-shargal@users.noreply.github.com>
2022-02-19 21:40:18 +0200

    Add files via upload

---
+++ Obstacles.py
@@ -0,0 +1,44 @@
+import csv
+
+
+class Obstacle:
+
+    def __init__(self,  num_of_points, x_meter, y_meter, z_meter, obs_type):
+
+        self.num_of_points = num_of_points
+        self.x_meter = x_meter
+        self.y_meter = y_meter
+        self.z_meter = z_meter
+        self.obs_type = obs_type
+
+    def get(self):
+        return self.num_of_points, self.x_meter, self.y_meter, self.z_meter, self.obs_type
+
+
+class Obstacles:
+
+    def __init__(self):
+        self._points_map = dict()
+        self._map = None
+
+    def read(self):
+        with open('obstacles_100m_above_sea_level.csv', newline='') as csvfile:
+            reader = csv.reader(csvfile, delimiter=',')
+            for row in reader:
+                obs = Obstacle(*row)
+                key = row[1], row[2], row[3]
+                self._points_map[key] = obs
+
+    def print(self):
+
+        for item in self._points_map.keys():
+            print(item)
+
+    def is_position_blocked(self, x, y, z):
+
+        key = x, y, z
+        return  self._points_map[key] is not None
+
+    def add_obstacle(self, x, y, z):
+
+        self._points_map[x, y, z] = Obstacle(None, x, y, z, None)
---
+++ main.py
@@ -0,0 +1,104 @@
+import math
+
+import numpy as np
+
+from DroneClient import DroneClient
+import time
+import airsim.utils
+import msvcrt
+import csv
+
+from DroneTypes import Position
+from Obstacles import Obstacles
+
+
+class MyDroneClient (DroneClient):
+
+    def __init__(self):
+        super().__init__()
+        self.stopped = False
+
+
+    def toCoords(self, pose):
+
+        x = pose.pos.x_m
+        y = pose.pos.y_m
+        z = pose.pos.z_m
+        return x, y, z
+
+    def stop(self):
+        if self.stopped == True:
+            return
+
+        self.stopped = True
+        pose_res = self.getPose()
+        x, y, z = self.toCoords(pose_res)
+        self.setAtPosition(x, y, z)
+
+    def resume(self):
+        super().flyToPosition(self.target_params)
+
+    def flyToPosition(self, x: float, y: float, z: float, v: float):
+        self.target_params = x, y, z, v
+        super().flyToPosition(x, y, z, v)
+
+    def senseObstacle(self):
+        lidar_data = self.getLidarData()
+        if lidar_data.points == [0.0]:
+            return False
+        return True
+
+
+
+
+def reached_goal_2D(curr: Position, goal: Position):
+
+    diff_x =  curr.x_m - goal.x_m
+    diff_y = curr.y_m - goal.y_m
+    dist = math.sqrt(diff_x*diff_x + diff_y*diff_y)
+
+    if dist < 11.0:
+        return True
+    return False
+
+
+
+
+if __name__ == "__main__":
+
+    obs = Obstacles()
+    obs.read()
+    obs.add_obstacle(10, 10, 99)
+    obs.print()
+
+
+    # client = MyDroneClient()
+    # client.connect()
+    #
+    # print(client.isConnected())
+    #
+    # time.sleep(4)
+    # client.setAtPosition(-346, -700, -100)
+    #
+    # time.sleep(3)
+    # goal = Position()
+    # goal.x_m, goal.y_m, goal.z_m = -346, -500, -100
+    # client.flyToPosition(goal.x_m, goal.y_m, goal.z_m, 5)
+    #
+    # while True:
+    #     lidar_data = client.getLidarData()
+    #     print("Lidar Data: "+str(lidar_data))
+    #
+    #     print("Position: ",str(client.getPose().pos ))
+    #
+    #     if reachedGoal2D(client.getPose().pos, goal):
+    #         print("Reached goal")
+    #         client.stop()
+    #
+    #     if client.senseObstacle():
+    #         print ("Found obstacle.")
+    #         client.stop()
+    #     time.sleep(1)
+
+
+        
