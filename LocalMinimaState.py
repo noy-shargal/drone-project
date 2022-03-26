58cdaeb26666a4915e2fc46d6e10d133b98ea286
ron alfia <ronalfia@campus.technion.ac.il>
2022-03-26 16:16:42 +0300

    wip

--- LocalMinimaState.py
+++ LocalMinimaState.py
@@ -21,7 +21,7 @@ class Vector:
         self._y /= norm
 
     def get_norm(self):
-        return math.sqrt(self._x**2 + self._y**2)
+        return math.sqrt(self._x ** 2 + self._y ** 2)
 
     def multiple_by_scalar(self, scalar):
         self._x *= scalar
@@ -31,7 +31,7 @@ class Vector:
         return self._x, self._y
 
     def inner_product(self, other):
-        return self._x*other._x + self._y*other._y
+        return self._x * other._x + self._y * other._y
 
     def remove_vector(self, other):
         self._x -= other._x
@@ -39,13 +39,16 @@ class Vector:
 
     def remove_projection(self, other):
         projection_scalar = self.inner_product(other) / self.get_norm()
-        projection_vector = Vector(Point(0,0), Point(self._x, self._y))
+        projection_vector = Vector(Point(0, 0), Point(self._x, self._y))
         projection_vector.multiple_by_scalar(projection_scalar)
 
-        output = Vector(Point(0,0), Point(other._x, other._y))
+        output = Vector(Point(0, 0), Point(other._x, other._y))
         output.remove_vector(projection_vector)
         output.normalize()
         return output
+
+    def get_as_point(self):
+        return Point(self._x, self._y)
 
 
 class LocalMinimaState(AlgoStateInterface):
@@ -57,21 +60,24 @@ class LocalMinimaState(AlgoStateInterface):
         print("ENTER LOCAL MINIMA STATE")
         # initialize
         self._stop()
-        m_line, d_min = self._calculate_m_line()  # LineString
+        m_line, d_min, start_position = self._calculate_m_line()  # LineString
         self._rotate_to_face_target(m_line)
         full_lidar_scan, world_cords_dict = self._agent.client.full_lidar_scan_v2()
-        vector_to_obstacle, direction_vector = self._initialize_vectors(full_lidar_scan, world_cords_dict, m_line)
-        while not self._crossed_M_line_at_lower_distance(m_line, d_min):
-            while not self._wall(full_lidar_scan, direction_vector) and self._wall(full_lidar_scan, vector_to_obstacle):
+        obstacle_vector, direction_vector = self._initialize_vectors(full_lidar_scan, world_cords_dict, m_line)
+        current_position = start_position
+        next_position = None
+        while not self._crossed_m_line_at_lower_distance(m_line, d_min, start_position, next_position,
+                                                         current_position):
+            while not self._wall(full_lidar_scan, direction_vector) and self._wall(full_lidar_scan, obstacle_vector):
                 full_lidar_scan = self._agent.client.full_lidar_scan()
                 next_position = self._calculate_next_position(direction_vector)
                 self._fly_to_position_and_wait(next_position)
             if self._wall(full_lidar_scan, direction_vector):
-                vector_to_obstacle, direction_vector = self._rotate_vectors_wall_ahead(vector_to_obstacle,
-                                                                                       direction_vector)
-            if not self._wall(full_lidar_scan, vector_to_obstacle):
-                vector_to_obstacle, direction_vector = self._rotate_vectors_wall_completed(vector_to_obstacle,
-                                                                                           direction_vector)
+                obstacle_vector, direction_vector = self._rotate_vectors_wall_ahead(obstacle_vector,
+                                                                                    direction_vector)
+            if not self._wall(full_lidar_scan, obstacle_vector):
+                obstacle_vector, direction_vector = self._rotate_vectors_wall_completed(obstacle_vector,
+                                                                                        direction_vector)
         return AlgoStateEnum.TRANSISTION
 
     def exit(self):
@@ -88,7 +94,7 @@ class LocalMinimaState(AlgoStateInterface):
         current_position = Point(pos.x_m, pos.y_m)
         line = Vector(current_position, target)
         d_min = current_position.distance(target)
-        return line, d_min
+        return line, d_min, current_position
 
     def _rotate_to_face_target(self, m_line):
         pass
@@ -100,11 +106,27 @@ class LocalMinimaState(AlgoStateInterface):
         pos = self._agent.client.getPose().pos
         current_position = Point(pos.x_m, pos.y_m)
         vector_to_obstacle = Vector(current_position, nearest_obs_point)
+        vector_to_obstacle.normalize()
 
         direction_vector = vector_to_obstacle.remove_projection(m_line)
 
         return vector_to_obstacle, direction_vector
 
-    def _crossed_M_line_at_lower_distance(self, m_line, d_min):
-        pass
+    def _crossed_m_line_at_lower_distance(self, m_line: Vector, d_min: float, start_position: Point,
+                                          next_position: Point,
+                                          current_position: Point,
+                                          threshold=1):
+        if next_position is None:
+            return False
+        target = self._agent.path[self._agent.astar_curr_point].point()
+        distance_to_target = next_position.distance(target)
+        if distance_to_target > (d_min - threshold):
+            return False
+        next_in_start = Point(next_position.x - start_position.x, next_position.y - start_position.y)
+        current_in_start = Point(current_position.x - start_position.x, current_position.y - start_position.y)
+
+        step_line_string = LineString([current_in_start, next_in_start])
+        m_line_string = LineString([Point(0, 0), m_line.get_as_point()])
+
+        return step_line_string.intersects(m_line_string)
 
